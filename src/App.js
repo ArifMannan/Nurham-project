6fc2874696cd5889f31792d6a2e4cbfa032617f9
Tariq <tariq.tvsl.2021@gmail.com>
2023-09-17 14:39:23 +0600

    add

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import logo from './logo.svg';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './component/homePage/Home';
 
 function App() {
   return (
     <>
+      <BrowserRouter basename="/app">
+        <Routes>
+          <Route path="/" element={<Home />} /> 
+          <Route path="/home" element={<Home />} /> 
+          {/* <Route path="/about" element={<Home />} />  */}
+        </Routes>
+      </BrowserRouter>
       <Home></Home>
     </>
   );
